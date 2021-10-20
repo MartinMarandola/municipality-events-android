2b2dd9ba0ae9f161a999b9e7ab6e149b533accf6
Martin Marándola <martinfmarandola@gmail.com>
2021-10-19 23:33:07 -0300

    initial commit: -Creacion de proyecto y splash screen

---
+++ app/src/main/java/com/example/municipalityevents/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.example.municipalityevents
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        var splashScreen = findViewById(R.id.iv_note) as ImageView
+        splashScreen.alpha = 0f;
+        splashScreen.animate().setDuration(2500).alpha(1f).withEndAction(){
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
